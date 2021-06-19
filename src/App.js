0781763a94ee93761bd84bc6ef1e797dc6fcfc60
lindomarc <lindomarc@outlook.com>
2021-06-19 18:04:37 -0300

    custom buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from 'react'
 import logo from "./logo.svg";
 import "./App.css";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Button from "@material-ui/core/Button";
-import SaveIcon from"@material-ui/icons/Save"
-import DeleteIcon from"@material-ui/icons/Delete"
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import TextField from "@material-ui/core/TextField";
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+import { green, orange } from '@material-ui/core/colors';
+
+ 
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FE8E53 )',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+const  theme = createMuiTheme({
+  palette:{
+    primary: {
+       main: green[500]
+    },
+    secondary: {
+       main: orange[500]
+    }
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExemple() {
   const [checked, setChecked] = React.useState(true)
   return(    
-    <FormControlLabel
-  
+    <FormControlLabel  
       control ={
         <Checkbox
           checked={checked}
@@ -32,28 +63,38 @@ function CheckboxExemple() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TextField />
-        <CheckboxExemple />
-        <ButtonGroup variant="contained" color="primary" >
-          <Button 
-            startIcon={<SaveIcon/>}
-            size="large">
-            Save
-          </Button>
-          <Button 
-            startIcon={<DeleteIcon/>}
-            size="large">
-            Discard
-          </Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+
+    <ThemeProvider theme={theme}> 
+      <div className="App">
+        <header className="App-header"> 
+          <ButtonStyled />
+          <TextField 
+            variant="outlined" 
+            color="secondary"
+            type="email"
+            label="the email"
+            placeholder="the email"
+          />
+          <CheckboxExemple />
+          <ButtonGroup variant="contained" color="primary" >
+            <Button 
+              startIcon={<SaveIcon/>}
+              size="large">
+              Save
+            </Button>
+            <Button 
+              startIcon={<DeleteIcon/>}
+              size="large">
+              Discard
+            </Button>
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    </ThemeProvider>  
   );
 }
 
